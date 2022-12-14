fa8773b9f0d1a3a495abe369078cbefdc02ff4e4
zaldeth <113926156+zaldeth@users.noreply.github.com>
2022-12-14 21:08:21 +0400

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,28 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+//create the rectangular shaped body.
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+    this.body = Bodies.rectangle(x, y, this.width, this.height, options);
+    World.add(world, this.body);
+    World.add(world, this.body);
+  }
+
+ 
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
